aedbd8bbe96bc5fead12e575df2b61ba85240da2
afit08 <53253309+afit08@users.noreply.github.com>
2024-06-05 18:37:10 +0700

    first commit

--- javascript.js
+++ javascript.js
@@ -30,3 +30,34 @@ $(document).ready(function () {
     }
   });
 });
+
+function sendEmail() {
+  const form = document.querySelector('form');
+  const email = document.getElementById('email').value;
+  const name = document.getElementById('name').value;
+  const message = document.getElementById('message').value;
+  const bodyMessage =
+    'email : ' + email + '<br> nama : ' + name + '<br> message : ' + message;
+
+  Email.send({
+    SecureToken: '3aed7a20-c5b1-4c4c-8c9c-dd532d14e732',
+    To: 'zafar2154.ez@gmail.com',
+    From: 'zafar2154.ez@gmail.com',
+    Subject: 'This is the subject',
+    Body: bodyMessage,
+  }).then((message) => {
+    if (message == 'OK') {
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'your message has been send',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  });
+}
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  sendEmail();
+});
